f92bb29e6abd2b7321dc5e16350bcb422862cb35
rmirzojonov <ravshanjonmir@gmail.com>
2018-04-29 16:48:31 +0300

    Finished

--- HexDecoder/Program.cs
+++ HexDecoder/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace HexDecoder
 {
@@ -10,6 +7,37 @@ namespace HexDecoder
     {
         static void Main(string[] args)
         {
+            foreach (var arg in args)
+            {
+                DecodeAndOutput(arg);
+            }
+            while (true)
+            {
+                Console.Write("Input data to decode: ");
+                string input = Console.ReadLine();
+                DecodeAndOutput(input);
+            }
+        }
+
+        private static void DecodeAndOutput(string input)
+        {
+            string decoded = DecodeHex(input);
+            if (!string.IsNullOrEmpty(decoded))
+                Console.WriteLine("Decoded: " + decoded);
+        }
+        public static string DecodeHex(string hex)
+        {
+            try
+            {
+                hex = hex.Replace("-", "");
+                byte[] raw = new byte[hex.Length / 2];
+                for (int i = 0; i < raw.Length; i++)
+                {
+                    raw[i] = Convert.ToByte(hex.Substring(i * 2, 2), 16);
+                }
+                return Encoding.BigEndianUnicode.GetString(raw);
+            }
+            catch { return null; }
         }
     }
 }
